365472cbf835402007aa39bcd8b2ecef3ed416d0
Nick Lyle <nicklyle2009@gmail.com>
2022-03-03 15:13:02 -0500

    created tentative format for file format, added some basic code formatting for getting started

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
