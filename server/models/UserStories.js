922c20d86de88d5da10b1fb08343315488061633
Charlie Money <cmoney5748@gmail.com>
2022-03-04 12:14:45 -0500

    user stories model

--- server/models/UserStories.js
+++ server/models/UserStories.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserStories = new Schema({
-
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+      message: {
+          type: String,
+          required: true
+      }
 })
