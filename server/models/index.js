5f9d94a282792c02bcd7e810415b516ca72b50c6
Michael Rawlings <rawlingsm99@gmail.com>
2022-03-04 09:42:11 -0600

    added categories to model files

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Student = require('./Clothes');
+const Course = require('./Furniture');
+const Course = require('./Kitchen');
+const Course = require('./Toys');
+const Course = require('./BabyFurniture')
+
+module.exports = { Student, Course };
