0cbc3ea7e69e2949e3504e84c13b8930e796cc34
Jake Finn <lof.steven@gmail.com>
2022-03-05 12:44:46 -0600

    Pull

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/be-a-neighbor',
   {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
 );
 
