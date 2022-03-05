37ce3a8fac2e93a63930b1d6a8d67a17f2536749
Jake Finn <lof.steven@gmail.com>
2022-03-05 11:34:20 -0600

    Add resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,57 +1,41 @@
 const { gql } = require('apollo-server-express');
 
+
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
     password: String
-    thoughts: [Thought]!
   }
 
-  type Thought {
+  type Post {
     _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
+    postAuthor: String
+    requestType: Boolean
+    postDescription: String
   }
 
-  type Comment {
+  type UserStory {
     _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
-
-  type Auth {
-    token: ID!
-    user: User
+    username: String
+    message: String
   }
 
   type Query {
     users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
-    me: User
+    posts: [Post]
+    userstories: [UserStory]
+    users(username: String!): User
+    post(postId: ID!): Post
+    userstory(userstortyID: ID!): UserStory
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-<<<<<<< HEAD
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addPost(postId: ID!): Post
+    addUserStory(userstortyID: ID!): UserStory
   }
-=======
-    addPost(postText: String!): Post
-    addComment(postId: ID!, commentText: String!): Post
-    removePost(postId: ID!): Post
-    removeComment(postId: ID!, commentId: ID!): Post
->>>>>>> b81cba7d711cf400565231a394479ba86c6b0697
 `;
 
-module.exports = typeDefs;
+module.export = typeDefs;
