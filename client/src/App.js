7612c8cceca923b0f2d30e431d4c85b6fc62a507
Jake Finn <lof.steven@gmail.com>
2022-03-05 14:40:46 -0600

    pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
-import Nav from './components/Nav';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Signup from "./components/Signup";
-import React from "react";
-import Header from "./components/Header";
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { render } from 'react-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 
+// import Header from './components/Header'
+import Home from './components/pages/Home';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+}); 
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <Header>
-        <Nav />
-      </Header>
-      {/* <Charities /> */}
-      {/* <Review />  */}
-      
+  render (
+    <div>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </ApolloProvider> 
     </div>
   );
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Carousel } from 'react-responsive-carousel';
+// import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
   return (
@@ -12,24 +12,11 @@ export default function Home() {
         </p>
       </div>
 
-
+{/* 
       <div>
         <h2 className='text-center fw-bold'>Testimonials</h2>
-        <Carousel>
-                <div>
-                    {/* <img src="assets/1.jpeg" /> */}
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    {/* <img src="assets/2.jpeg" /> */}
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    {/* <img src="assets/3.jpeg" /> */}
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
-      </div>
+
+      </div> */}
     </div>
 
   )
