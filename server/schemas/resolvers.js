6e64f7440cfccdafc8ae715a6a0b42d13f86662e
Charlie Money <cmoney5748@gmail.com>
2022-03-05 12:13:29 -0500

    commit before merge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,20 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Post } = require('../models');
+const { User, Post, UserStory } = require('../models');
 const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        users: async () => {
+        return await User.find({})
+      },
+      posts: async () => {
+        return await Post.find({});
+      },
+      userstories: async () => {
+        return await UserStory.find({});
+      }
+    }
+  };
+  
+  module.exports = resolvers;
