1978adc492251a5d987b0ce2df9a49b4bd678583
Jake Finn <lof.steven@gmail.com>
2022-03-05 19:48:59 -0600

    Add login/signup pages

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,13 +1,89 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import { Link } from 'react-router-dom';
-// import { LOGIN } from '../utils/mutations';
-// import Auth from '../utils/auth';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../../utils/mutations';
 
-// function Login(props) {
-//     return(
-        
-//     )
-// }
+import Auth from '../../utils/auth';
 
-// export default Login;
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    //   <div>slayyyy queen</div>
+    <main>
+        <h1>Login</h1>
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+    </main>
+  );
+};
+
+export default Login;
