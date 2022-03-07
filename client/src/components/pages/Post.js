58a4a3679b16a701f00e80714910d67eeb0b7a10
Jake Finn <lof.steven@gmail.com>
2022-03-07 17:07:54 -0600

    pull

--- client/src/components/pages/Post.js
+++ client/src/components/pages/Post.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../Sidebar';
+
+import PostList from '../PostList'
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div>
+      {/* <Sidebar/> */}
+      <PostList/>
+    </div>
   )
 }
