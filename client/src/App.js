89c67c2ac2291edf7ce04f764cf074c0149ff894
Michael Rawlings <rawlingsm99@gmail.com>
2022-03-07 15:57:24 -0600

    google maps added to Post page

--- client/src/App.js
+++ client/src/App.js
@@ -44,14 +44,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-// This is Google Maps code from line .
-
-// const location = {
-//   address: '1600 Amphitheatre Parkway, Mountain View, california.',
-//   lat: 37.42216,
-//   lng: -122.08427,
-// }
-// console.log(client)
 
 function App() {
   return (
