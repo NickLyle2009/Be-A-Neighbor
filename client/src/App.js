dfc36e7d254d4d7addb26efdb95fa29cb11a4ee2
Bart Gloyd <bartgloyd@gmail.com>
2022-03-07 12:06:35 -0500

    Checklist

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
@@ -19,21 +19,24 @@ import Profile from './components/pages/Profile';
 import Give from './components/pages/Give';
 // import Map from './components/pages/Map';
 import Footer from './components/Footer'
-import Viewreq from './components/pages/Viewreq';
+import Post from './components/pages/Post';
 import Request from './components/pages/Request';
+
+
+
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
-}); 
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
@@ -41,19 +44,17 @@ const client = new ApolloClient({
 });
 
 // This is Google Maps code from line .
- 
 
 // const location = {
 //   address: '1600 Amphitheatre Parkway, Mountain View, california.',
 //   lat: 37.42216,
 //   lng: -122.08427,
-// } 
+// }
 // console.log(client)
 
 function App() {
   return (
     <div>
-
       <ApolloProvider client={client}>
       <Header/>
       <Navigation/>
@@ -63,14 +64,15 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/me' element={<Profile/>}/>
+          <Route path='/profiles' element={<Profile/>}/>
           <Route path='/give' element={<Give/>}/>
           <Route path='/request' element={<Request/>}/>
-          <Route path='/viewrequests' element={<Viewreq/>}/>
+          <Route path='/posts' element={<Post/>}/>
           {/* <Route path='/map' element={<Map/>}/> */}
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <Footer/>
-      </ApolloProvider> 
+        <Footer />
+      </ApolloProvider>
     </div>
   );
 }
